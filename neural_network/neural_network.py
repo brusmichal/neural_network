2391eb4cc2312d2dd66168cbb2003824e2030aa5
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2022-05-19 14:37:26 +0200

    There was an attempt

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -6,11 +6,15 @@ class Network(object):
     def __init__(self, neurons_per_layer):
         self.number_of_layers = len(neurons_per_layer)
         self.neurons_per_layer = neurons_per_layer
-        rng = default_rng()
-        self.biases = [rng.uniform(-1 / np.sqrt(neurons_per_layer[0]), 1 / np.sqrt(neurons_per_layer[0]), size=(y, 1)) \
-                       for y in neurons_per_layer[1:]]
-        self.weights = [rng.uniform(-1 / np.sqrt(neurons_per_layer[0]), 1 / np.sqrt(neurons_per_layer[0]), size=(y, x)) \
-                        for x, y in zip(neurons_per_layer[:-1], neurons_per_layer[1:])]
+        self.rng = default_rng()
+        inv_sqrt_first_layer = np.sqrt(neurons_per_layer[0])
+        self.biases = [
+            self.rng.uniform(-inv_sqrt_first_layer, inv_sqrt_first_layer, size=(y, 1)) for y in neurons_per_layer
+        ]
+        self.weights = [
+            self.rng.uniform(-inv_sqrt_first_layer, inv_sqrt_first_layer, size=(y, x))
+            for x, y in zip(reversed(neurons_per_layer), neurons_per_layer)
+        ]
 
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
@@ -18,12 +22,11 @@ class Network(object):
         return a
 
     def train_with_sgd(self, training_data, epochs, mini_batch_size, learning_rate, test_data=None):
-        if test_data:
+        if test_data is not None:
             n_test = len(test_data)
         n = len(training_data)
         for i in range(epochs):
-            rng = default_rng()
-            rng.shuffle(training_data)
+            self.rng.shuffle(training_data)
             mini_batches = [training_data[k:k + mini_batch_size] for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, learning_rate)
@@ -36,21 +39,25 @@ class Network(object):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
 
-        for x, y in mini_batch:
-            delta_nabla_b, delta_nabla_w = self.backpropagation(x, y)
-            nabla_b = [nb + d_nb for nb, d_nb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw + d_nw for nw, d_nw in zip(nabla_w, delta_nabla_w)]
-        self.biases = [b - (learning_rate / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
-        self.weights = [w - (learning_rate / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
+        for pixels, label in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backpropagation(pixels, label)
+            nabla_b = np.array([nb + d_nb for nb, d_nb in zip(nabla_b, delta_nabla_b)])
+            nabla_w = np.array([nw + d_nw for nw, d_nw in zip(nabla_w, delta_nabla_w)])
+            print(f'LABEL: {label}')
+        self.biases = np.array([b - (learning_rate / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)])
+        self.weights = np.array([w - (learning_rate / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)])
 
     def backpropagation(self, x, y):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
 
         activation = x
+        print(f'LEN X ===== {np.size(x)}')
         activations = [x]
         z_vectors = []
         for b, w in zip(self.biases, self.weights):
+            print(f'LEN B ===== {np.size(b)}')
+            print(f'LEN W ===== {np.size(w)}')
             z_vector = np.dot(w, activation) + b
             z_vectors.append(z_vector)
             activation = relu(z_vector)
@@ -60,11 +67,11 @@ class Network(object):
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
 
-        for l in range(2, self.number_of_layers):
-            z_vector = z_vectors[-l]
-            delta = np.dot(self.weights[-l + 1].transpose(), delta) * relu_derivative(z_vector)
-            nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+        for layer in range(2, self.number_of_layers):
+            z_vector = z_vectors[-layer]
+            delta = np.dot(self.weights[-layer + 1].transpose(), delta) * relu_derivative(z_vector)
+            nabla_b[-layer] = delta
+            nabla_w[-layer] = np.dot(delta, np.transpose(np.array(activations[-layer - 1])))
 # tu wyżej jest gdzieś błąd, próbowałem użyć np array zamiast [] i nadal nie działa
         return nabla_b, nabla_w
 
